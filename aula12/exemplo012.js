f675e76c3ffe4dbd1e91e00fc8aa48b0022c086b
GabrielGrilloB <gabriel.g_benevides@outlook.com>
2024-07-30 19:01:13 -0300

    switch

---
+++ aula12/exemplo012.js
@@ -0,0 +1,11 @@
+var idade = 78
+console.log(`Você tem ${idade} anos`)
+if( idade < 16 ) {
+  console.log(`Nâo vota`)
+} else if (idade >=16 && idade <18 || idade> 65){
+  console.log(`Voto opcional`)
+  }
+  if(idade >=18 && idade<65){
+    console.log(`Voto obrigatorio`)
+  }
+
