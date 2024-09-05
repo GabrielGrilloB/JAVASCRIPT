5496e7ae6173926afa796205c95308e1de3e2f3b
GabrielGrilloB <gabriel.g_benevides@outlook.com>
2024-09-05 14:46:10 -0300

    arrow

---
+++ funções/funcoes.js
@@ -0,0 +1,40 @@
+/*
+   Função: Trecho de código que só é executado quando é chamado/invocado
+
+      Função Void (vazia)
+      Função com Parâmetro(s)
+      Função Return
+      Função Arrow / Arrow Function
+
+*/
+/*  
+             Função Void
+
+let valor = 20
+
+function incrementa() {
+valor = valor + 35
+}
+
+incrementa()
+console.log(valor)
+*/
+
+/*
+        Função Return
+
+function soma(numero1, numero2){
+    const somaDosNumeros = numero1 + numero2
+
+    return somaDosNumeros
+}
+const meusNumeros = soma(4,6)
+
+console.log(meusNumeros)
+*/
+
+const multi = (numero1, numero2) => numero1 * numero2
+
+const mp = multi(2,5)
+
+console.log(mp)
